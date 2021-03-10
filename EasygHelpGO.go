8dc3be489d829eb520111eadcbb02350359acbc4
Eduardo Barbosa (an4kein) <37910997+an4kein@users.noreply.github.com>
2021-03-10 07:30:45 -0800

    Update EasygHelpGO.go
    Ajustes corretos realizados, utilizacao correta de canais

--- EasygHelpGO.go
+++ EasygHelpGO.go
@@ -7,31 +7,29 @@ import (
 	"strings"
 )
 
-var perguntas = []string{"", "1 - Problemas com wifi", "2 - Problemas com minha internet", "3 - Segunda Via da Fatura"}
-var options int
+var perguntas1 = []string{"Escolha uma opcao entre 1-3", "1 - Problemas com wifi", "2 - Problemas com minha internet", "3 - Segunda Via da Fatura"}
+var options1 int
+var respostas1 = []string{"Escolha uma opcao entre 1-3", "Option 1: \nDesligue o seu roteador por 5 minutos",
+	"Option 2: \n Verifique se o roteador encontra-se ligado e com os cabos conectados",
+	"Option 3: \n Acesse o link http://example.com e imprima a segunda via"}
 
 func main() {
 	fmt.Println("Enter your option: ")
-	fmt.Println(strings.Join(perguntas, "\n"))
-	fmt.Scanln(&options)
+	fmt.Println(strings.Join(perguntas1, "\n"))
+	fmt.Scanln(&options1)
 	meucanal := make(chan []string)
-	go send(meucanal)
-	receiver(meucanal)
+	go send1(meucanal)
+	receiver1(meucanal)
 }
 
-func send(s chan<- []string) {
-	for _, v := range perguntas {
-		lm := []string{v}
-		s <- lm
-	}
+func send1(s chan<- []string) {
+	s <- respostas1
 }
 
-func receiver(r <-chan []string) {
-	if options == 1 {
-		fmt.Println("Option 1: \nDesligue o seu roteador por 5 minutos")
-	} else if options == 2 {
-		fmt.Println("Option 2: \n Verifique se o roteador encontra-se ligado e com os cabos conectados")
-	} else if options == 3 {
-		fmt.Println("Option 3: \n Acesse o link http://example.com e imprima a segunda via")
+func receiver1(r <-chan []string) {
+	for i, v := range <-r {
+		if i == options1 {
+			fmt.Println(v)
+		}
 	}
 }
